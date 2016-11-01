fdda27ea348f608014045ef712d0278944104b07
JVAparicio <aparicio.jv@gmail.com>
2016-11-01 19:29:24 +0000

    Add files via upload

--- src/Uniplaces/STest/ListingFinderSearchTypeAdvanced.php
+++ src/Uniplaces/STest/ListingFinderSearchTypeAdvanced.php
@@ -14,8 +14,8 @@ final class ListingFinderSearchTypeAdvanced extends ListingFinder
     protected function handleSearchType($listing, array $search)
     {	 
         if(!$this->checkAdress($listing, $search)) {
-		    return false;
-	    }		
+		   return false;
+        }		
 	    return $this->checkPrice($listing, $search);
     }
   
@@ -27,34 +27,34 @@ final class ListingFinderSearchTypeAdvanced extends ListingFinder
      */
     private function checkAdress($listing, $search) 
     {
-		if (isset($search['address'])) {
-                $listingAddress = strtolower(trim($listing->getLocalization()->getAddress()));
-                $address = strtolower(trim($search['address']));
+	    if (isset($search['address'])) {
+            $listingAddress = strtolower(trim($listing->getLocalization()->getAddress()));
+            $address = strtolower(trim($search['address']));
 					 
-                if (levenshtein($listingAddress, $address) > 5) {
-                    return false;
-                }			
+            if (levenshtein($listingAddress, $address) > 5) {
+                return false;
+            }			
         }		
 	    return true;
     }
 	
-	/**
+    /**
      * @param Listing $listing
      * @param array    $search
      *
      * @return bool
      */
 	private function checkPrice($listing, $search) 
-	{
-		if (isset($search['price'])) {
-                $listingPrice = $listing->getPrice();
-				$minPricing = isset($search['price']['min']) ? $search['price']['min'] : null;
-                $maxPricing = isset($search['price']['max']) ? $search['price']['max'] : null;
+    {
+        if (isset($search['price'])) {
+            $listingPrice = $listing->getPrice();
+			$minPricing = isset($search['price']['min']) ? $search['price']['min'] : null;
+            $maxPricing = isset($search['price']['max']) ? $search['price']['max'] : null;
 
-                if (($minPricing !== null && $minPricing > $listingPrice) || ($maxPricing !== null && $maxPricing < $listingPrice)) {
-                    return false;
-                }
-		}
-		return true;
-	}
+            if (($minPricing !== null && $minPricing > $listingPrice) || ($maxPricing !== null && $maxPricing < $listingPrice)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
